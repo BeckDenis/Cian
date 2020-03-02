1429505f41b59b33947f7f32f261e54873ffd986
Denis <57283152+BeckDenis@users.noreply.github.com>
2020-03-02 14:44:38 +0600

    created project

---
+++ app/src/main/java/com/example/cian/main/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.example.cian.main
+
+
+import androidx.fragment.app.Fragment
+import com.example.cian.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+
+
+}
